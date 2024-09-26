fee47a2d14f948e75d81c48e2b51e4641350e701
yahya momin <yahyamomin06@gmail.com>
2024-09-26 17:38:25 +0530

    add vercel.json

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import parserRoutes from "./routes/routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use("/api/v1/", parserRoutes);
 
 export default app;
